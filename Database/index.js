42d082a57ee820f314edd3396ed921231a54696b
BJ White <bjwhite0512@gmail.com>
2022-03-03 19:54:33 -0500

    setup a connection to my postgres db using pg library

---
+++ Database/index.js
@@ -0,0 +1,27 @@
+const { Pool, Client } = require('pg');
+const password = require('../config.js');
+
+const pool = new Pool({
+  user: 'data',
+  host: 'localhost',
+  database: 'Products',
+  password: password,
+  port: 5432,
+})
+
+module.exports = pool;
+
+// const client = new Client({
+//    user: 'data',
+//    host: 'localhost',
+//    database: 'Products',
+//    password: password,
+//    port: 5432,
+// })
+// client.connect()
+// client.query('Select * from products where id=1', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
+
+// module.exports = client;
