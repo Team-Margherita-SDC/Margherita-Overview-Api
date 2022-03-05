0c69bf91c3a9e437fc18ec5b76866197300ca1fd
BJ White <bjwhite0512@gmail.com>
2022-03-05 10:27:16 -0500

    requests to endpoints for products, product info, styles are working

--- server/index.js
+++ server/index.js
@@ -1,10 +1,53 @@
 const express = require('express');
-
+const db = require('../Database/index.js');
+const { getProducts, getProductInfo, getStyles, getRelated } = require('../Database/dbQueryFuncs/queries.js')
 const app = express();
 const port = 3200;
 
 app.use(express.json());
 
+app.get('/products', (req, res) => {
+  count = req.query.count || 5;
+  page = req.query.page || 1;
+
+  getProducts(count, page, (err, results) => {
+    if (err) {
+      res.status(400).send('Error Retrieving Products')
+    } else {
+      res.status(200).send(results);
+    }
+  })
+})
+
+app.get('/products/:product_id', (req, res) => {
+  getProductInfo(req.params.product_id, (err, results) => {
+    if (err) {
+      res.status(400).send('Error Retrieving Product Info')
+    } else {
+      res.status(200).send(results);
+    }
+  })
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  getStyles(req.params.product_id, (err, results) => {
+    if (err) {
+      res.status(400).send('Error Retrieving Styles');
+    } else {
+      res.status(200).send(results);
+    }
+  });
+})
+
+app.get('/products/:product_id/related', (req, res) => {
+  getRelated(req.params.product_id, (err, results) => {
+    if (err) {
+      res.status(400).send('Error Retrieving Related Product Info');
+    } else {
+      res.status(200).send(results);
+    }
+  });
+})
 
 
 app.listen(port, () => {
